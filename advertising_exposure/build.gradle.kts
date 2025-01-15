e2850e2508564842b08cdd1423d1d7a171ff17c4
tangpoo <lhinav3@gmail.com>
2025-01-15 18:41:02 +0900

    #1 Feat : Advertising_Exposure : MySQL, ElasticSearch 의존성 추가 및 YML 설정

--- advertising_exposure/build.gradle.kts
+++ advertising_exposure/build.gradle.kts
@@ -1,1 +1,6 @@
 
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.mysql:mysql-connector-j")
+}
