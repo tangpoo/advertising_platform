900034dde707c183f1195cfd8d63de4e5473262e
tangpoo <lhinav3@gmail.com>
2025-01-30 21:05:16 +0900

    #1 Feat : Advertising_Exposure : contextLoad Test 컨테이너 적용

--- advertising_billing/src/test/kotlin/com/advertising/advertising_billing/AdvertisingBillingApplicationTests.kt
+++ advertising_billing/src/test/kotlin/com/advertising/advertising_billing/AdvertisingBillingApplicationTests.kt
@@ -1,10 +1,38 @@
 package com.advertising.advertising_billing
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 
+@Testcontainers
 @SpringBootTest
 class AdvertisingBillingApplicationTests {
+    companion object {
+
+        @Container
+        val mysqlContainer = MySQLContainer(DockerImageName.parse("mysql:latest"))
+
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll() {
+            mysqlContainer.start()
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun configure(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { mysqlContainer.jdbcUrl }
+            registry.add("spring.datasource.username") { mysqlContainer.username }
+            registry.add("spring.datasource.password") { mysqlContainer.password }
+            registry.add("spring.datasource.driver-class-name") { mysqlContainer.driverClassName }
+        }
+    }
 
     @Test
     fun contextLoads() {
